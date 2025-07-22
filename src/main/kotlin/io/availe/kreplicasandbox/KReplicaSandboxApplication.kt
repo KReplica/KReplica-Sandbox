d36ee176a869891186a3bd4eddcc0a1197bf15bf
availe <94802525+availe@users.noreply.github.com>
2025-07-22 17:32:17 -0400

    project init

---
+++ src/main/kotlin/io/availe/kreplicasandbox/KReplicaSandboxApplication.kt
@@ -0,0 +1,11 @@
+package io.availe.kreplicasandbox
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KReplicaSandboxApplication
+
+fun main(args: Array<String>) {
+    runApplication<KReplicaSandboxApplication>(*args)
+}
---
+++ src/test/kotlin/io/availe/kreplicasandbox/KReplicaSandboxApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.availe.kreplicasandbox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KReplicaSandboxApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
